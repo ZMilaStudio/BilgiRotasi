6a3097b6d00738eb1d0f6ba7dd14aefe735eea76
ZMilaStudio <levent.ua@gmail.com>
2026-09-12 02:03:35 +0300

    test(kelime-avi): prove one generic themed map renders all skins

--- test/word_hunt_route_visual_theme_test.dart
+++ test/word_hunt_route_visual_theme_test.dart
@@ -1,7 +1,7 @@
-import 'dart:ui';
-
 import 'package:bilgi_rotasi/word_hunt/word_hunt_reusable_route_map_screen.dart';
 import 'package:bilgi_rotasi/word_hunt/word_hunt_route_visual_theme.dart';
+import 'package:bilgi_rotasi/word_hunt/word_hunt_starter_content.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -29,6 +29,41 @@ void main() {
     for (final theme in WordHuntRouteVisualThemeProofs.all) {
       expect(theme.mapTheme.id, isNotEmpty);
       expect(WordHuntRouteMapGeometry.pointsFor(size), canonical);
+    }
+  });
+
+  testWidgets('one generic themed screen renders forest sky and harbor', (
+    tester,
+  ) async {
+    tester.view.physicalSize = const Size(390, 844);
+    tester.view.devicePixelRatio = 1;
+    addTearDown(() {
+      tester.view.resetPhysicalSize();
+      tester.view.resetDevicePixelRatio();
+    });
+
+    for (final visualTheme in WordHuntRouteVisualThemeProofs.all) {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: WordHuntThemedRouteMapScreen(
+            route: WordHuntStarterContent.baslangicLimani,
+            visualTheme: visualTheme,
+          ),
+        ),
+      );
+      await tester.pump();
+
+      expect(
+        find.byKey(const Key('word_hunt_reusable_route_map')),
+        findsOneWidget,
+      );
+      for (var level = 1; level <= 10; level++) {
+        expect(
+          find.byKey(Key('word_hunt_reusable_level_$level')),
+          findsOneWidget,
+        );
+      }
+      expect(tester.takeException(), isNull);
     }
   });
 }
