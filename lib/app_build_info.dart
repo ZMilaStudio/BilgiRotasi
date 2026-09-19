9130c90da603623fc1d667a33f91d4139f8d76ad
ZMilaStudio <levent.ua@gmail.com>
2026-09-19 19:26:10 +0300

    chore(release): sync AppBuildInfo to 1.68.21+111

--- lib/app_build_info.dart
+++ lib/app_build_info.dart
@@ -3,8 +3,8 @@ part of 'main.dart';
 class AppBuildInfo {
   AppBuildInfo._();
 
-  static const String versionName = '1.68.20';
-  static const int buildNumber = 110;
+  static const String versionName = '1.68.21';
+  static const int buildNumber = 111;
 
   static const String firebaseEnvironment = String.fromEnvironment(
     'FIREBASE_ENVIRONMENT',
@@ -15,7 +15,7 @@ class AppBuildInfo {
       ? 'Development'
       : 'Production';
 
-  static const String version = '1.68.20+110';
+  static const String version = '1.68.21+111';
   static const String fullLabel = 'Sürüm $version • $channel';
   static const String compactLabel = 'Bilgi Rotası • $versionName • $channel';
 }
