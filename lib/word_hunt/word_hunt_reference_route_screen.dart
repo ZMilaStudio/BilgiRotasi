0d72ac8320e092a56324cdc9c404a7feee3618ed
ZMilaStudio <levent.ua@gmail.com>
2026-09-21 01:14:20 +0300

    fix(word-hunt): repair reference map stack closure

--- lib/word_hunt/word_hunt_reference_route_screen.dart
+++ lib/word_hunt/word_hunt_reference_route_screen.dart
@@ -265,7 +265,6 @@ class WordHuntReferenceRouteScreen extends StatelessWidget {
                               ],
                             ),
                           ),
-                            ),
                         ],
                       ),
                     ),
